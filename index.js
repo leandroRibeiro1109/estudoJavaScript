ea64c0b0b3f24d5cd54ce7faa7f27493a8a3d5c0
Leandro Ribeiro <leandroribeiro1109@gmail.com>
2019-09-01 22:56:11 -0300

    estudo de funcoes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('arquivo carregado')
+function imc (altura, peso) {
+    return peso / (altura * altura)
+}
+
+var pessoas = [
+    {
+        nome: 'pedro',
+        altura: 1.83,
+        peso: 80
+    },
+    {
+        nome: 'maria',
+        altura: 1.90,
+        peso: 90
+    },
+    {
+        nome: 'joao',
+        altura: 2.00,
+        peso:100
+    }            
+]
+
+for (var pesssoa of pessoas) {
+    var resultado = imc(pesssoa.altura, pesssoa.peso)
+
+    console.log(pesssoa.nome, resultado)
+}
